4f5aa938bae0479cbc8c0129ae81f398075cfc7e
Sanika Hukkeri <sanikajhukkeri@gmail.com>
2026-03-23 10:19:55 +0530

    Added login page

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
+
 console.log("Trying to connect...");
+
 // MongoDB Connection
 mongoose.connect("mongodb+srv://student-course-cluster:655bO0BBHipCqmtI@student-course-cluster.llprb6r.mongodb.net/student-course-registration?retryWrites=true&w=majority&authSource=admin")
 .then(() => console.log("MongoDB Connected"))
@@ -12,13 +15,13 @@ mongoose.connect("mongodb+srv://student-course-cluster:655bO0BBHipCqmtI@student-
 app.use(express.json());
 app.use(express.static('public'));
 
-// Route
+// Route → show login page
 app.get('/', (req, res) => {
-    res.send("Student Course Registration System Running");
+    res.sendFile(path.join(__dirname, 'views', 'login.html'));
 });
 
 // Server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
